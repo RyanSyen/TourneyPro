969e9b46086f5ff35cf29a1201aa5148a8f0974f
Ryan Wong <ryanwong710200@gmail.com>
2024-02-03 12:13:23 +0800

    added url to header in middleware + added template

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,25 @@
-export { default } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
-export const config = { matcher: ["/protected"] };
+export { default, withAuth } from "next-auth/middleware";
+
+// export const config = { matcher: ["/protected"] };
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  //   console.log("middleware req header url: ", request.url);
+
+  // store current req url in custom header to be read later
+  //! currently not using - previously wanted to be used to render based on url but this is only applicable for server side, if a page redirects then middleware wont be able to detect
+  const reqHeaders = new Headers(request.headers);
+  reqHeaders.set("x-url", request.url);
+
+  return NextResponse.next({
+    headers: reqHeaders,
+  });
+}
+
+// export withAuth(
+//     function middleware(req: NextRequest){
+//         console.log(req.nextauth.token)
+//     }
+// )
