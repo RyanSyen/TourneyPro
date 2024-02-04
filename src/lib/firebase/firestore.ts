612a733eb62b96da9ccb32da6aa0f1cfb1cae4f4
Ryan Wong <ryanwong710200@gmail.com>
2024-02-04 09:11:38 +0800

    edit firestore private key

--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -5,6 +5,8 @@ export const firestore = initFirestore({
   credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+      : undefined,
   }),
 });
