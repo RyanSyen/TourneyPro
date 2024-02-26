c46fbcdfc19e17bca366127f1506cbc8fbb1a3ba
Ryan Wong <ryanwong710200@gmail.com>
2024-02-26 22:14:04 +0800

    migrated styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // define hostnames for next/image component
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+      },
+      {
+        hostname: "images.t2u.io",
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  // sass compiler config
+  sassOptions: {
+    // includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
 export default nextConfig;
