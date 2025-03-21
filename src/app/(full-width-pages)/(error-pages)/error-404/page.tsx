26ab447268bce2f5da1c2540b4bbf497650a9f78
ryan.wong <ryanwong710200@gmail.com>
2025-03-22 00:36:18 +0800

    updated 404 footer, changed tailadmin to tourneypro

--- src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
+++ src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
@@ -47,7 +47,7 @@ export default function Error404() {
       </div>
       {/* <!-- Footer --> */}
       <p className="absolute text-sm text-center text-gray-500 -translate-x-1/2 bottom-6 left-1/2 dark:text-gray-400">
-        &copy; {new Date().getFullYear()} - TailAdmin
+        &copy; {new Date().getFullYear()} - TourneyPro
       </p>
     </div>
   );
