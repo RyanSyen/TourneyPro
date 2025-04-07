9354ae09ae19c0f81dd8e21eab28bd8a88072b51
ryan.wong <ryanwong710200@gmail.com>
2025-04-07 17:55:38 +0800

    modified sign in button

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -164,9 +164,11 @@ const AppHeader: React.FC = () => {
           {/* <!-- User Area --> */}
           {/* <UserDropdown /> */}
           <SignedOut>
-            <div className="px-4 py-3 text-sm inline-flex items-center justify-center font-medium gap-2 rounded-lg transition text-nowrap bg-white text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-white/[0.03] dark:hover:text-gray-300">
-              <SignInButton />
-            </div>
+            <SignInButton>
+              <div className="cursor-pointer px-4 py-3 text-sm inline-flex items-center justify-center font-medium gap-2 rounded-lg transition text-nowrap bg-white text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-white/[0.03] dark:hover:text-gray-300">
+                Sign in
+              </div>
+            </SignInButton>
           </SignedOut>
           <SignedIn>
             <UserButton
