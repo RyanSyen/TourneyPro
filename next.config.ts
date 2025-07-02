e84f39bbb06104c8b51c0ef14a7a068a39fe93bf
ryan.wong <ryanwong710200@gmail.com>
2025-07-02 23:50:51 +0800

    configurered turbopack + enabled serverComponentsHmrCache + setup detailed logging

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,26 @@ import withBundleAnalyzer from "@next/bundle-analyzer";
 const withAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
   openAnalyzer: true, // auto open the report in the browser
-})
+});
+
+const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack"],
+  //   });
+  //   return config;
+  // },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
   },
   images: {
     remotePatterns: [
@@ -30,6 +40,16 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
+  },
+  experimental: {
+    serverComponentsHmrCache: isDev,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true, // Log full URL for fetch requests
+      hmrRefreshes: true, // Log HMR refreshes
+    },
+    incomingRequests: false, // Disable logging of incoming requests
   }
 };
 
