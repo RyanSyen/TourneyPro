a7cb7975783c6ffaab4a6884d7c14ba63eb6249b
ryan.wong <ryanwong710200@gmail.com>
2025-07-02 17:46:37 +0800

    Use React's cache function to avoid unnecessary duplicate requests to the database during a render pass

--- src/services/requireAuthUser.ts
+++ src/services/requireAuthUser.ts
@@ -1,8 +1,20 @@
 import { headers } from "next/headers";
 import { prisma } from "@/lib/prisma";
 import { auth } from "../../auth";
+import { cache } from "react";
 
-export async function requireAuthUser() {
+// nextjs best practices
+/*
+- A DAL can be used to protect data fetched at request time. However, for static routes that share data between users,
+data will be fetched at build time and not at request time. Use Middleware to protect static routes.
+
+- For secure checks, you can check if the session is valid by comparing the session ID with your database.
+Use React's cache function to avoid unnecessary duplicate requests to the database during a render pass.
+
+- You may wish to consolidate related data requests in a JavaScript class that runs verifySession() before any methods.
+*/
+
+export const requireAuthUser = cache(async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -22,4 +34,4 @@ export async function requireAuthUser() {
   }
 
   return user;
-}
+});
