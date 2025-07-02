c0bcc4b80133bf57889349e519e13c0478f3517c
ryan.wong <ryanwong710200@gmail.com>
2025-07-02 15:51:42 +0800

    completed user profile dropdown + logout

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,22 @@ const nextConfig: NextConfig = {
     });
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  }
 };
 
 export default withAnalyzer(nextConfig);
---
+++ src/components/user-profile/UserNavDropdown.tsx
@@ -0,0 +1,93 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { authClient } from "@/lib/auth-client";
+import { Router } from "next/router";
+import { useRouter } from "next/navigation";
+
+interface props {
+  username?: string;
+  profilePic?: string;
+}
+
+export function UserNavDropdown({ username, profilePic }: props) {
+  console.log("UserNavDropdown rendered with:", {
+    username,
+    profilePic,
+  });
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      await authClient.signOut({
+        fetchOptions: {
+          onSuccess: () => {
+            router.push("/signin");
+          },
+        },
+      });
+    } catch (err) {
+      console.error("Failed to sign out:", err);
+    }
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-9 w-9">
+            <AvatarImage
+              src={profilePic}
+              alt={username ? username : "User Avatar"}
+              referrerPolicy="no-referrer" // google profile image block cross-origin image embeds unless referrer is suppressed, this prevents Google from regecting the request from the originating URL
+            />
+            <AvatarFallback>{username ? username[0] : "SC"}</AvatarFallback>
+          </Avatar>
+          {/* <span className="hidden lg:inline-block text-gray-800 dark:text-white">
+            {username || "Guest"}
+          </span> */}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm leading-none font-medium">{username}</p>
+            <p className="text-muted-foreground text-xs leading-none">
+              this will be their role here
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>New Team</DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleSignOut}>
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
